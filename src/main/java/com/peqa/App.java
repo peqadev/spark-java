9e12eaa2d53f5ca99783b5b5eaca02fa14f684c4
Erick Agrazal <erick.agrazal@gmail.com>
2023-11-26 12:53:09 -0500

    Initial commit

---
+++ src/main/java/com/peqa/App.java
@@ -0,0 +1,9 @@
+package com.peqa;
+
+import static spark.Spark.*;
+
+public class App {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World - Spark");
+    }
+}
